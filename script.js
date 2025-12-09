22b14b98e3b728b25a8877a6ed96be372e1c0038
Misa Valencia <misavalmad@gmail.com>
2025-12-09 10:14:31 -0600

    Add exercise 4

--- script.js
+++ script.js
@@ -40,4 +40,15 @@ function sumAndProduct(arr) {
 }
 sumAndProduct([1, 2, 3, 4]);
 
+/*Exercise #4 (sugerencia filter() e includes())
+let student1Courses = ['Math', 'English', 'Programming'];
+let student2Courses = ['Geography', 'Spanish', 'Programming'];
+Create a program that loops over the 2 arrays; if there are any common courses print them out to the console.*/
+
+function commonCourses(arr1, arr2) {
+  let common = arr1.filter(course => arr2.includes(course));
+  console.log('Common courses: ' + common.join(', '));
+}
+commonCourses(['Math', 'English', 'Programming'], ['Geography', 'Spanish', 'Programming']);
+
 
